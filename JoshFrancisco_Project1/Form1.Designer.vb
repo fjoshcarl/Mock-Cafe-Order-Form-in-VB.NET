59129a2589ce573c4ca040e713429bf231fa3aa3
Josh Francisco <fjoshcarl@yahoo.com>
2019-07-11 15:30:47 +0800

    Add to repo

---
+++ JoshFrancisco_Project1/Form1.Designer.vb
@@ -0,0 +1,372 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.cbmenu = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.btnvoid = New System.Windows.Forms.Button()
+        Me.btncheckout = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txttotal = New System.Windows.Forms.TextBox()
+        Me.lbfood = New System.Windows.Forms.ListBox()
+        Me.lbcost = New System.Windows.Forms.ListBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(38, 34)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(104, 90)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(235, 190)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Label1"
+        Me.Label1.Visible = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(235, 241)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Label2"
+        Me.Label2.Visible = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Location = New System.Drawing.Point(235, 296)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Label3"
+        Me.Label3.Visible = False
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Location = New System.Drawing.Point(235, 351)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        Me.Label4.Visible = False
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Location = New System.Drawing.Point(235, 408)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Label5"
+        Me.Label5.Visible = False
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Castellar", 26.25!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Chocolate
+        Me.Label6.Location = New System.Drawing.Point(148, 55)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(247, 42)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "CAFIZ CAFE"
+        '
+        'cbmenu
+        '
+        Me.cbmenu.FormattingEnabled = True
+        Me.cbmenu.Items.AddRange(New Object() {"Breakfast", "Lunch", "Dinner"})
+        Me.cbmenu.Location = New System.Drawing.Point(225, 103)
+        Me.cbmenu.Name = "cbmenu"
+        Me.cbmenu.Size = New System.Drawing.Size(121, 21)
+        Me.cbmenu.TabIndex = 7
+        Me.cbmenu.Text = "Select Menu Type"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(38, 183)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox1.TabIndex = 8
+        Me.TextBox1.Visible = False
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(38, 234)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox2.TabIndex = 9
+        Me.TextBox2.Visible = False
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(38, 293)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox3.TabIndex = 11
+        Me.TextBox3.Visible = False
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(38, 348)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Visible = False
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(38, 405)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(41, 20)
+        Me.TextBox5.TabIndex = 12
+        Me.TextBox5.Visible = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(102, 172)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 41)
+        Me.Button1.TabIndex = 13
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(102, 227)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(98, 41)
+        Me.Button2.TabIndex = 14
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        Me.Button2.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(102, 282)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(98, 41)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(102, 337)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(98, 41)
+        Me.Button4.TabIndex = 16
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        Me.Button4.Visible = False
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(102, 394)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(98, 41)
+        Me.Button5.TabIndex = 17
+        Me.Button5.Text = "Button5"
+        Me.Button5.UseVisualStyleBackColor = True
+        Me.Button5.Visible = False
+        '
+        'btnvoid
+        '
+        Me.btnvoid.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnvoid.Location = New System.Drawing.Point(85, 467)
+        Me.btnvoid.Name = "btnvoid"
+        Me.btnvoid.Size = New System.Drawing.Size(132, 66)
+        Me.btnvoid.TabIndex = 18
+        Me.btnvoid.Text = "VOID"
+        Me.btnvoid.UseVisualStyleBackColor = True
+        Me.btnvoid.Visible = False
+        '
+        'btncheckout
+        '
+        Me.btncheckout.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btncheckout.Location = New System.Drawing.Point(429, 493)
+        Me.btncheckout.Name = "btncheckout"
+        Me.btncheckout.Size = New System.Drawing.Size(138, 41)
+        Me.btncheckout.TabIndex = 19
+        Me.btncheckout.Text = "Bill Checkout"
+        Me.btncheckout.UseVisualStyleBackColor = True
+        Me.btncheckout.Visible = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("Lucida Handwriting", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Black
+        Me.Label7.Location = New System.Drawing.Point(377, 156)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(175, 21)
+        Me.Label7.TabIndex = 20
+        Me.Label7.Text = "Order Summary:"
+        Me.Label7.Visible = False
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.BackColor = System.Drawing.Color.Transparent
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(385, 451)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(54, 20)
+        Me.Label8.TabIndex = 21
+        Me.Label8.Text = "Total:"
+        Me.Label8.Visible = False
+        '
+        'txttotal
+        '
+        Me.txttotal.Location = New System.Drawing.Point(445, 451)
+        Me.txttotal.Name = "txttotal"
+        Me.txttotal.Size = New System.Drawing.Size(122, 20)
+        Me.txttotal.TabIndex = 22
+        Me.txttotal.Visible = False
+        '
+        'lbfood
+        '
+        Me.lbfood.FormattingEnabled = True
+        Me.lbfood.Location = New System.Drawing.Point(381, 190)
+        Me.lbfood.Name = "lbfood"
+        Me.lbfood.Size = New System.Drawing.Size(125, 238)
+        Me.lbfood.TabIndex = 23
+        Me.lbfood.Visible = False
+        '
+        'lbcost
+        '
+        Me.lbcost.FormattingEnabled = True
+        Me.lbcost.Location = New System.Drawing.Point(525, 190)
+        Me.lbcost.Name = "lbcost"
+        Me.lbcost.Size = New System.Drawing.Size(125, 238)
+        Me.lbcost.TabIndex = 24
+        Me.lbcost.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(679, 580)
+        Me.Controls.Add(Me.lbcost)
+        Me.Controls.Add(Me.lbfood)
+        Me.Controls.Add(Me.txttotal)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btncheckout)
+        Me.Controls.Add(Me.btnvoid)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cbmenu)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents cbmenu As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents btnvoid As Button
+    Friend WithEvents btncheckout As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txttotal As TextBox
+    Friend WithEvents lbfood As ListBox
+    Friend WithEvents lbcost As ListBox
+End Class
---
+++ JoshFrancisco_Project1/Form1.vb
@@ -0,0 +1,136 @@
+﻿Public Class Form1
+    Dim fd1, fd2, fd3, fd4, fd5, cost, total As Double
+
+    Private Sub Cbmenu_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cbmenu.SelectedIndexChanged
+        If cbmenu.SelectedIndex = 0 Then
+            Button1.Text = "Pancake"
+            Label1.Text = "Php 110.00"
+            fd1 = 110
+            Button2.Text = "Hotdog"
+            Label2.Text = "Php 40.00"
+            fd2 = 40
+            Button3.Text = "Bread"
+            Label3.Text = "Php 45.00"
+            fd3 = 45
+            Button4.Text = "Cake"
+            Label4.Text = "Php 75.00"
+            fd4 = 75
+            Button5.Text = "Egg"
+            Label5.Text = "Php 20.00"
+            fd5 = 20
+        ElseIf cbmenu.SelectedIndex = 1 Then
+            Button1.Text = "Chicken"
+            Label1.Text = "Php 150.00"
+            fd1 = 150
+            Button2.Text = "Beef and Mushroom"
+            Label2.Text = "Php 170.00"
+            fd2 = 170
+            Button3.Text = "Pork Steak"
+            Label3.Text = "Php 120.00"
+            fd3 = 120
+            Button4.Text = "Spaghetti"
+            Label4.Text = "Php 90.00"
+            fd4 = 90
+            Button5.Text = "Rice"
+            Label5.Text = "Php 30.00"
+            fd5 = 30
+        ElseIf cbmenu.SelectedIndex = 2 Then
+            Button1.Text = "Chicken"
+            Label1.Text = "Php 150.00"
+            fd1 = 150
+            Button2.Text = "Beef and Mushroom"
+            Label2.Text = "Php 170.00"
+            fd2 = 170
+            Button3.Text = "Pork Adobo"
+            Label3.Text = "Php 130.00"
+            fd3 = 130
+            Button4.Text = "Rice"
+            Label4.Text = "Php 30.00"
+            fd4 = 30
+            Button5.Text = "Chocolate"
+            Label5.Text = "Php 250.00"
+            fd5 = 250
+        End If
+        TextBox1.Show()
+        TextBox2.Show()
+        TextBox3.Show()
+        TextBox4.Show()
+        TextBox5.Show()
+        txttotal.Show()
+        Button1.Show()
+        Button2.Show()
+        Button3.Show()
+        Button4.Show()
+        Button5.Show()
+        btnvoid.Show()
+        btncheckout.Show()
+        Label1.Show()
+        Label2.Show()
+        Label3.Show()
+        Label4.Show()
+        Label5.Show()
+        Label7.Show()
+        Label8.Show()
+        lbcost.Show()
+        lbfood.Show()
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+    End Sub
+
+    Private Sub Btnvoid_Click(sender As Object, e As EventArgs) Handles btnvoid.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+        TextBox4.Clear()
+        TextBox5.Clear()
+        lbcost.Items.Clear()
+        lbfood.Items.Clear()
+        txttotal.Clear()
+        total = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lbfood.Items.Add(Val(TextBox1.Text) & " " & Button1.Text)
+        cost = Val(TextBox1.Text) * fd1
+        lbcost.Items.Add(cost)
+        total = total + cost
+        txttotal.Text = total
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        lbfood.Items.Add(Val(TextBox2.Text) & " " & Button2.Text)
+        cost = Val(TextBox2.Text) * fd2
+        lbcost.Items.Add(cost)
+        total = total + cost
+        txttotal.Text = total
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        lbfood.Items.Add(Val(TextBox3.Text) & " " & Button3.Text)
+        cost = Val(TextBox3.Text) * fd3
+        lbcost.Items.Add(cost)
+        total = total + cost
+        txttotal.Text = total
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        lbfood.Items.Add(Val(TextBox4.Text) & " " & Button4.Text)
+        cost = Val(TextBox4.Text) * fd4
+        lbcost.Items.Add(cost)
+        total = total + cost
+        txttotal.Text = total
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        lbfood.Items.Add(Val(TextBox5.Text) & " " & Button5.Text)
+        cost = Val(TextBox5.Text) * fd5
+        lbcost.Items.Add(cost)
+        total = total + cost
+        txttotal.Text = total
+    End Sub
+
+
+End Class
